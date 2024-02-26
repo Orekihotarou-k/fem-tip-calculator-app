04f88b7653e2cad834a36611ec5023b899204011
Levis Kim <122298089+Orekihotarou-k@users.noreply.github.com>
2024-02-26 15:31:01 +0100

    update script.js: calculator works

--- script.js
+++ script.js
@@ -1,70 +1,75 @@
-// get elements and assign them as constants
-const selectAmount = document.querySelectorAll(".selectAmount")
+// get elements
+const MAX_CUSTOM_INPUT = 99
+
 const billInput = document.getElementById("billInput")
-const peopleInput = document.getElementById("peopleInput")
 const customInput = document.getElementById("customInput")
+const tipSelectButton = document.querySelectorAll(".selectAmount")
+const peopleInput = document.getElementById("peopleInput")
 const personTotal = document.querySelector(".person")
 const overallTotal = document.querySelector(".total")
-const resetBtn = document.querySelector(".reset-btn")
+const resetButton = document.querySelector(".reset-btn")
+const errorMessage = document.querySelector(".error-message")
 
-function calculateTip( inputValue, billAmount, peopleCount) {
-    const inputNumber = parseFloat(inputValue)
 
-    if (!isNaN(inputNumber) && peopleCount > 0 && billAmount > 0) {
-        const inputPercentage = inputNumber / 100
-        const tipAmountPerson = (billAmount * inputPercentage) / peopleCount
+// function to calculate tip
+function calculateTip(selectedTipPercentage, billAmount, peopleCount) {
+    const tipAmount = parseInt(selectedTipPercentage)
+
+    if (!isNaN(tipAmount) && billAmount > 0 && peopleCount > 0 && tipAmount <= MAX_CUSTOM_INPUT) {
+        const tipPercentage = tipAmount / 100
+        const tipAmountPerson = (billAmount * tipPercentage) / peopleCount
         const tipAmountTotal = (billAmount / peopleCount) + tipAmountPerson
-        return  {tipAmountPerson, tipAmountTotal}
+
+        return {tipAmountPerson, tipAmountTotal}
     }
 
-    return { tipAmountPerson: 0, tipAmountTotal: 0}
+    return { tipAmountPerson: 0, tipAmountTotal:0 }
 }
 
-selectAmount.forEach(input => {
+// handle the custom input
+customInput.addEventListener("input", () => {
+    const customInputValue = parseInt(customInput.value)
+
+     if (!isNaN(customInputValue) && customInputValue <= MAX_CUSTOM_INPUT) {
+        const {tipAmountPerson, tipAmountTotal} = calculateTip(customInputValue, billInput.value, parseInt(peopleInput.value))
+
+        personTotal.innerText = "$" + tipAmountPerson.toFixed(2)
+        overallTotal.innerText = "$" + tipAmountTotal.toFixed(2)
+    }
+    
+})
+
+
+// handle the tip select button input
+tipSelectButton.forEach(input => {
     input.addEventListener("click", () => {
-        const { tipAmountPerson, tipAmountTotal } = calculateTip( input.textContent, billInput.value, parseInt(peopleInput.value ))
-            personTotal.innerText = "$" + tipAmountPerson.toFixed(2)
-            overallTotal.innerText = "$" + tipAmountTotal.toFixed(2)
+        const {tipAmountPerson, tipAmountTotal} = calculateTip(input.textContent, billInput.value, parseInt(peopleInput.value))
+
+        personTotal.innerText = "$" + tipAmountPerson.toFixed(2)
+        overallTotal.innerText = "$" + tipAmountTotal.toFixed(2)
     })
 })
 
 
+// Check peopleInput on input change and toggle error message visibility
+peopleInput.addEventListener("input", () => {
+    const peopleInputValue = parseInt(peopleInput.value);
 
-
-// reset the calculator
-resetBtn.addEventListener("click", () => {
-    peopleInput.value = ""
-    billInput.value = ""
-    personTotal.innerText = "$0.00"
-    overallTotal.innerText = "$0.00"
+    if (peopleInputValue === 0) {
+        errorMessage.style.visibility = "visible"
+        peopleInput.style.outlineColor = "#ee5353"
+    } else {        
+        errorMessage.style.visibility = "hidden"
+        peopleInput.style.outlineColor = "transparent"
+    }
 })
 
 
-
-
-
-
-// selectAmount.forEach(input => {
-//     input.addEventListener("click", () => {
-//         // convert input value to percentage
-//         const inputNumber = parseFloat(input.textContent)
-
-//         // check if the number is valid
-//         if (!isNaN(inputNumber)) {
-//             const inputPercentage = (inputNumber / 100).toFixed(2)
-            
-//             // get the billinput
-//             peopleInputNumber = parseInt(peopleInput.value)
-//             billInputAmount = parseFloat(billInput.value)
-//             tipAmountPerson = billInputAmount * inputPercentage / peopleInputNumber
-//             tipAmountTotal = billInputAmount / peopleInputNumber + tipAmountPerson
-
-//             // change the value of the person total 
-//             personTotal.innerText = "$" + tipAmountPerson.toFixed(2)
-//             overallTotal.innerText = "$" + tipAmountTotal.toFixed(2)
-//         }
-
-//     })
-// })
-
-
+// reset everything 
+resetButton.addEventListener("click", () => {
+    customInput.value = ""
+    billInput.value = ""
+    peopleInput.value = ""
+    personTotal.innerText = "$0.00"
+    overallTotal.innerText = "$0.00"
+})
